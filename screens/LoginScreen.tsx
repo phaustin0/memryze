0f8c28e715df66faecd786cf42f926ac03a0b02f
phaustin0 <zechy14@icloud.com>
2021-12-06 18:40:59 +0530

    implemented login functionality

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import useAuth from "../hooks/useAuth";
+import { db } from "../firebase";
+import { SafeAreaView, Text, TouchableOpacity } from "react-native";
+import { LoginScreenProps as Props } from "../types";
 
-const LoginScreen = () => {
+const LoginScreen = ({ route, navigation }: Props) => {
+  const { loading, signInWithGoogle } = useAuth();
+  const signIn = async () => await signInWithGoogle();
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView style={{ alignItems: "center" }}>
+      <Text>{loading ? "loading" : "login"}</Text>
+      <TouchableOpacity style={{ marginTop: 10 }} onPress={signIn}>
+        <Text style={{ color: "#246de0" }}>log in with google</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
